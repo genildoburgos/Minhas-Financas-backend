584d0043a10b32fd9142922b4c8c5b42e1f0e705
genildoburgos <gnldburgos@gmail.com>
2024-04-06 17:27:54 -0300

    Refatorando entidades adicionando as anotações do Lombok

--- src/main/java/com/genildo/minhasfinancas/model/entity/Lancamento.java
+++ src/main/java/com/genildo/minhasfinancas/model/entity/Lancamento.java
@@ -1,6 +1,10 @@
 package com.genildo.minhasfinancas.model.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import java.math.BigDecimal;
@@ -9,6 +13,10 @@ import java.util.Objects;
 
 @Entity
 @Table (name = "lancamento", schema = "financas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
 public class Lancamento {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
@@ -38,105 +46,6 @@ public class Lancamento {
     @Column ( name = "tipo")
     @Enumerated (value = EnumType.STRING )
     private TipoLancamento tipo;
-
-    @Override
-    public String toString() {
-        return "Lancamento{" +
-                "id=" + id +
-                ", descricao='" + descricao + '\'' +
-                ", mes=" + mes +
-                ", ano=" + ano +
-                ", usuario=" + usuario +
-                ", valor=" + valor +
-                ", dataCadastro=" + dataCadastro +
-                ", tipo=" + tipo +
-                ", status=" + status +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Lancamento that)) return false;
-        return Objects.equals(getId(), that.getId()) && Objects.equals(getDescricao(), that.getDescricao()) && Objects.equals(getMes(), that.getMes()) && Objects.equals(getAno(), that.getAno()) && Objects.equals(getUsuario(), that.getUsuario()) && Objects.equals(getValor(), that.getValor()) && Objects.equals(getDataCadastro(), that.getDataCadastro()) && getTipo() == that.getTipo() && getStatus() == that.getStatus();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getDescricao(), getMes(), getAno(), getUsuario(), getValor(), getDataCadastro(), getTipo(), getStatus());
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public Integer getMes() {
-        return mes;
-    }
-
-    public void setMes(Integer mes) {
-        this.mes = mes;
-    }
-
-    public Integer getAno() {
-        return ano;
-    }
-
-    public void setAno(Integer ano) {
-        this.ano = ano;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-
-    public BigDecimal getValor() {
-        return valor;
-    }
-
-    public void setValor(BigDecimal valor) {
-        this.valor = valor;
-    }
-
-    public LocalDate getDataCadastro() {
-        return dataCadastro;
-    }
-
-    public void setDataCadastro(LocalDate dataCadastro) {
-        this.dataCadastro = dataCadastro;
-    }
-
-    public TipoLancamento getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoLancamento tipo) {
-        this.tipo = tipo;
-    }
-
-    public StatusLancamento getStatus() {
-        return status;
-    }
-
-    public void setStatus(StatusLancamento status) {
-        this.status = status;
-    }
 
     @Column ( name = "status")
     @Enumerated (value = EnumType.STRING)
